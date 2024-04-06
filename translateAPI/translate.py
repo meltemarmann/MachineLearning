7c39917447c9f010748b470374585bafe66d4379
NumanAvci <n.avci@etu.edu.tr>
2024-04-06 13:09:54 +0300

    Google translate function created

---
+++ translateAPI/translate.py
@@ -0,0 +1,36 @@
+from google.cloud import translate
+
+EN = "en-US"
+TR = "tr"
+PROJECT_ID = "valid-flow-412916"
+
+def translate_text(text="Hello, world!", source_language="en-US", target_language="tr"):
+
+    client = translate.TranslationServiceClient()
+    location = "global"
+    parent = f"projects/{PROJECT_ID}/locations/{location}"
+
+    response = client.translate_text(
+        request={
+            "parent": parent,
+            "contents": [text],
+            "mime_type": "text/plain",
+            "source_language_code": source_language,
+            "target_language_code": target_language,
+        }
+    )
+    for translation in response.translations:
+        return translation.translated_text
+    
+
+translate_text(text="", source_language=EN, target_language=TR)
+
+"""
+{
+  "translations": [
+    {
+      "translatedText": "TRANSLATED_TEXT",
+    }
+  ]
+}
+"""
